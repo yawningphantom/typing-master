2b46003fee3442ff3182700a27ba68b2e792c43e
siddharthsinghgithub <siddharth@sugoilabs.com>
2020-08-12 23:44:35 +0530

    Updates

---
+++ precache-manifest.78fe61db4fe4911bbff47570fa7f5252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18a77f02e134dd6f4ecdea7c25da216",
+    "url": "/typing-master/index.html"
+  },
+  {
+    "revision": "05bf438179f29cb6e10d",
+    "url": "/typing-master/static/css/main.bf24ffd6.chunk.css"
+  },
+  {
+    "revision": "b15a907fac9a7fe50056",
+    "url": "/typing-master/static/js/2.a353f5fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-master/static/js/2.a353f5fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bf438179f29cb6e10d",
+    "url": "/typing-master/static/js/main.fa89e9d7.chunk.js"
+  },
+  {
+    "revision": "a1a89a68b45b1dd5894d",
+    "url": "/typing-master/static/js/runtime-main.858d0f20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typing-master/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0affc1038a6b5893e08e128aa0e83ca.js"
+  "/typing-master/precache-manifest.78fe61db4fe4911bbff47570fa7f5252.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typing-master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
